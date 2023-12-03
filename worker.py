51c3c60231dc6ab177c01a2a4f49f5abf38fdc1c
Zeb Becker <zbecker2@bowdoin.edu>
2023-12-03 11:25:35 -0500

    Move model defintion into external file

--- worker.py
+++ worker.py
@@ -31,18 +31,20 @@ from torch.optim import Adam
 
 import numpy as np
 
+import worker_model
+
 PORT = 8000
 BATCH_SIZE = 128
-LEARNING_RATE = .001
+LEARNING_RATE = 0.001
+
+EPOCHS_PER_UPDATE = 1
 
 # Check if GPU is available, and use if possible, data is sent to "device" later
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 class Worker:
-
     def __init__(self, ip_address):
-        
         # Set up RPC server to receive notifications
         self.hostname = "http://" + ip_address + ":" + str(PORT)
         self.server = SimpleXMLRPCServer((ip_address, PORT))
@@ -54,18 +56,22 @@ class Worker:
         self.model = None
         self.optimizer = None
         self.loss = None
-        self.epochs = 1
+        self.epochs = EPOCHS_PER_UPDATE
         self.global_epoch = 0
 
         # Set up data using PyTorch DataLoaders
-        # Just a helpful object for splitting up data 
+        # Just a helpful object for splitting up data
         self.train_dl = None
         self.test_dl = None
 
         # Can always switch to local files later,
         # Gonna put this here for now (like every worker has the entire dataset)
-        mnist_train = MNIST('~/data', train=True, download=True, transform=transforms.ToTensor())
-        mnist_test = MNIST('~/data', train=False, download=True, transform=transforms.ToTensor())
+        mnist_train = MNIST(
+            "~/data", train=True, download=True, transform=transforms.ToTensor()
+        )
+        mnist_test = MNIST(
+            "~/data", train=False, download=True, transform=transforms.ToTensor()
+        )
         self.train_dl = DataLoader(mnist_train, batch_size=BATCH_SIZE, shuffle=True)
         self.test_dl = DataLoader(mnist_test, batch_size=BATCH_SIZE, shuffle=True)
 
@@ -75,7 +81,7 @@ class Worker:
         # Start up worker server in seperate thread
         self.server.register_function(self.receive_notification, "notify")
         server_thread = threading.Thread(target=self.server.serve_forever)
-        server_thread.start()     
+        server_thread.start()
 
         # Connect to host and greet with intro message
         self.coordinator = xmlrpc.client.ServerProxy(hostname)
@@ -86,18 +92,10 @@ class Worker:
             print("Error: Unable to connect to", hostname)
             raise e
 
-        # Initialize model with info from coordinator
-        self.model = nn.Sequential(
-            nn.Conv2d(1, 64, 3, 1, 1), # in_ch, out_ch, k, stride, pad
-            nn.ReLU(),
-            nn.Conv2d(64, 4, 3, 1, 1),
-            nn.ReLU(),
-            nn.Flatten(),
-            nn.Linear(3136, 10)
-        ).to(device)  # Assuming the worker knows the model format...
+        # Initialize model as it is defined in worker_model.py
+        self.model = worker_model.model.to(device)
         self.optimizer = Adam(self.model.parameters(), lr=LEARNING_RATE)
         self.loss = nn.CrossEntropyLoss().to(device)
-
 
     def update_weights(self, coordinator_weights):
         """Update PyTorch Model from raw weights"""
@@ -106,35 +104,35 @@ class Worker:
         new_state = old_state.copy()
         for name, new_weights in zip(old_state, coordinator_weights):
             old_tensor = new_state[name]
-            new_tensor = torch.tensor(new_weights, dtype=old_tensor.dtype, device=device)
+            new_tensor = torch.tensor(
+                new_weights, dtype=old_tensor.dtype, device=device
+            )
             new_state[name] = new_tensor
 
         self.model.load_state_dict(new_state)
 
-
     def train_batch(self, x, y):
         """Given data and labels for a batch, learn better weights"""
 
-        self.optimizer.zero_grad()       # Flush memory
-        pred = self.model(x)             # Get predictions
+        self.optimizer.zero_grad()  # Flush memory
+        pred = self.model(x)  # Get predictions
         batch_loss = self.loss(pred, y)  # Compute loss
-        batch_loss.backward()            # Compute gradients
-        self.optimizer.step()            # Make a GD step
+        batch_loss.backward()  # Compute gradients
+        self.optimizer.step()  # Make a GD step
         return batch_loss.detach().cpu().numpy()
 
     def train(self):
         """Run through local training data for a bit
-        
-        Runs for as many epochs as is specified in self.epochs. Each 
+
+        Runs for as many epochs as is specified in self.epochs. Each
         epoch is split up into batches for SGD and shuffling the data.
         Returns the new weights for the model as nested list
         """
-        
+
         print("Training with data", self.data)
         loss_history = []
         start = time.time()
         for epoch in range(self.epochs):
-
             print(f"Running Epoch {epoch + 1} of {self.epochs}")
             epoch_losses = []
             for batch in self.train_dl:
@@ -148,20 +146,20 @@ class Worker:
 
         end = time.time()
         training_time = end - start
-        print(f"Loss History: {loss_history}")    # Could be useful if we want to plot loss later
+        print(
+            f"Loss History: {loss_history}"
+        )  # Could be useful if we want to plot loss later
         print(f"Training Time: {training_time}")  # Could be useful for tests later
-        
-        return [param.data.tolist() for param in self.model.parameters()]
 
+        return [param.data.tolist() for param in self.model.parameters()]
 
     def receive_notification(self):
         self.update_ready = True
 
     def wait_for_notification(self):
-        
         # Wait for server thread to register an update
         while not self.update_ready:
-            time.sleep(.01)
+            time.sleep(0.01)
 
         # Reset update status for later
         self.update_ready = False
@@ -170,7 +168,6 @@ class Worker:
         """Main loop for working with coordinator"""
 
         while True:
-
             # Get caught up to date with coordinator
             try:
                 update, epoch = self.coordinator.Coordinator.get_update()
@@ -188,7 +185,7 @@ class Worker:
                     print(f"Coordinator could not use update: {status}")
                     break
                 self.wait_for_notification()
-                
+
             except Exception as e:
                 print(f"Problem while training: {e}")
                 break
@@ -200,9 +197,8 @@ class Worker:
 
 
 def main():
-
     print(f"Running on {device}")
-    
+
     # Get hostname from command line "http://<hostname>:<port>"
     name = sys.argv[1]
     hostname = "http://" + name
