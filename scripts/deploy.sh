5dfc0713826f8b14d837eb0cf5c7cbcebdd05619
Zeb Becker <zbecker2@bowdoin.edu>
2023-12-11 10:58:40 -0500

    Also copy coordinator program to test coordinator node

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,8 @@
 # Run from within scripts directory
 echo "Starting deployment." 
+scp -i ~/.ssh/$(whoami)-keypair.pem ../src/coordinator.py $(whoami)@15.156.205.154:~
+echo "Finished copying coordinator program."
+echo "Copied worker program to:"
 for server in $(cat ../serverlist.txt)
 do 
   scp -i ~/.ssh/$(whoami)-keypair.pem ../src/worker.py ../src/worker_model.py ../worker_requirements.txt $(whoami)@$server:~ &
