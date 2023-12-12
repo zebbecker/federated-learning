14e39d4ce623c90e18250ea755d7b756b5fe976a
Zeb Becker <zbecker2@bowdoin.edu>
2023-12-12 16:32:26 -0500

    Fix to avoid hanging on failed workers

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,16 +1,11 @@
 # ssh into each worker machine, start worker program
-# with appropriate command line args (or seek them in a config file?)
-
-function start_server() 
-{
-  ssh -i ~/.ssh/$(whoami)-keypair.pem $(whoami)@$1 "python3 worker.py 15.156.205.154:8082 $1 &"
-}
+# with appropriate command line args
 
 echo ">>> Starting workers."
 
 for server in $(cat ../fullserverlist.txt)
 do 
-  start_server ${server} &
+  ssh -i ~/.ssh/$(whoami)-keypair.pem $(whoami)@$server "python3 worker.py 15.156.205.154:8082 $server" &
   echo ${server}
 done  
 echo  ">>> Finished starting workers."
