9c33af3d2c1bbcb5035a13c6b56ebe740ab14e14
Alex Racapé <alex.racape@gmail.com>
2023-12-14 13:26:00 -0500

    Randomized Data Subset

--- src/worker.py
+++ src/worker.py
@@ -4,6 +4,7 @@ import socket
 import xmlrpc.client
 from xmlrpc.server import SimpleXMLRPCServer
 import threading
+import random
 
 import torch
 import torch.nn as nn
@@ -78,7 +79,14 @@ class Worker:
         mnist_test = MNIST(
             "~/data", train=False, download=True, transform=transforms.ToTensor()
         )
-        self.train_dl = DataLoader(mnist_train, batch_size=BATCH_SIZE, shuffle=True)
+
+        # Get random sized subset
+        set_size = random.randint(100, len(mnist_train))
+        random_set = set(random.sample(range(1, len(mnist_train)), set_size))
+        subset = torch.utils.data.Subset(mnist_train, random_set)
+        print(f"Working with dataset of {set_size} images")
+
+        self.train_dl = DataLoader(subset, batch_size=BATCH_SIZE, shuffle=True)
         self.test_dl = DataLoader(mnist_test, batch_size=BATCH_SIZE, shuffle=True)
 
     def connect(self):
